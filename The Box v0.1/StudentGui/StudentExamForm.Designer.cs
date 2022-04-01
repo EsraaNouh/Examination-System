845bbb121f6382c332cf37aa7d5cbccbff620b21
ahmedsamy1234 <48600639+ahmedsamy1234@users.noreply.github.com>
2022-04-02 00:56:53 +0200

    add timer to Exam

--- The Box v0.1/StudentGui/StudentExamForm.Designer.cs
+++ The Box v0.1/StudentGui/StudentExamForm.Designer.cs
@@ -33,30 +33,34 @@
             this.Start_Exam_Btn = new System.Windows.Forms.Button();
             this.NextQ = new System.Windows.Forms.Button();
             this.Submit_Exam_Btn = new System.Windows.Forms.Button();
+            this.TimerLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ChooseCourselabel
             // 
             this.ChooseCourselabel.AutoSize = true;
-            this.ChooseCourselabel.Location = new System.Drawing.Point(62, 45);
+            this.ChooseCourselabel.Location = new System.Drawing.Point(46, 37);
+            this.ChooseCourselabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ChooseCourselabel.Name = "ChooseCourselabel";
-            this.ChooseCourselabel.Size = new System.Drawing.Size(105, 17);
+            this.ChooseCourselabel.Size = new System.Drawing.Size(79, 13);
             this.ChooseCourselabel.TabIndex = 0;
             this.ChooseCourselabel.Text = "Choose Course";
             // 
             // ChooseCourse
             // 
             this.ChooseCourse.FormattingEnabled = true;
-            this.ChooseCourse.Location = new System.Drawing.Point(234, 42);
+            this.ChooseCourse.Location = new System.Drawing.Point(176, 34);
+            this.ChooseCourse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ChooseCourse.Name = "ChooseCourse";
-            this.ChooseCourse.Size = new System.Drawing.Size(121, 24);
+            this.ChooseCourse.Size = new System.Drawing.Size(92, 21);
             this.ChooseCourse.TabIndex = 1;
             // 
             // Start_Exam_Btn
             // 
-            this.Start_Exam_Btn.Location = new System.Drawing.Point(436, 38);
+            this.Start_Exam_Btn.Location = new System.Drawing.Point(327, 31);
+            this.Start_Exam_Btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Start_Exam_Btn.Name = "Start_Exam_Btn";
-            this.Start_Exam_Btn.Size = new System.Drawing.Size(106, 31);
+            this.Start_Exam_Btn.Size = new System.Drawing.Size(80, 25);
             this.Start_Exam_Btn.TabIndex = 2;
             this.Start_Exam_Btn.Text = "Start Exam";
             this.Start_Exam_Btn.UseVisualStyleBackColor = true;
@@ -64,9 +68,10 @@
             // 
             // NextQ
             // 
-            this.NextQ.Location = new System.Drawing.Point(325, 410);
+            this.NextQ.Location = new System.Drawing.Point(244, 333);
+            this.NextQ.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.NextQ.Name = "NextQ";
-            this.NextQ.Size = new System.Drawing.Size(115, 28);
+            this.NextQ.Size = new System.Drawing.Size(86, 23);
             this.NextQ.TabIndex = 3;
             this.NextQ.Text = "Next Question";
             this.NextQ.UseVisualStyleBackColor = true;
@@ -74,24 +79,37 @@
             // 
             // Submit_Exam_Btn
             // 
-            this.Submit_Exam_Btn.Location = new System.Drawing.Point(706, 411);
+            this.Submit_Exam_Btn.Location = new System.Drawing.Point(530, 334);
+            this.Submit_Exam_Btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Submit_Exam_Btn.Name = "Submit_Exam_Btn";
-            this.Submit_Exam_Btn.Size = new System.Drawing.Size(75, 27);
+            this.Submit_Exam_Btn.Size = new System.Drawing.Size(56, 22);
             this.Submit_Exam_Btn.TabIndex = 4;
             this.Submit_Exam_Btn.Text = "Submit";
             this.Submit_Exam_Btn.UseVisualStyleBackColor = true;
             this.Submit_Exam_Btn.Click += new System.EventHandler(this.Submit_Exam_Btn_Click);
             // 
+            // TimerLabel
+            // 
+            this.TimerLabel.AutoSize = true;
+            this.TimerLabel.Location = new System.Drawing.Point(461, 37);
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.Size = new System.Drawing.Size(35, 13);
+            this.TimerLabel.TabIndex = 5;
+            this.TimerLabel.Text = "label1";
+            this.TimerLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
             // StudentExamForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.TimerLabel);
             this.Controls.Add(this.Submit_Exam_Btn);
             this.Controls.Add(this.NextQ);
             this.Controls.Add(this.Start_Exam_Btn);
             this.Controls.Add(this.ChooseCourse);
             this.Controls.Add(this.ChooseCourselabel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "StudentExamForm";
             this.Text = "StudentExamForm";
             this.Load += new System.EventHandler(this.StudentExamForm_Load);
@@ -107,5 +125,6 @@
         private System.Windows.Forms.Button Start_Exam_Btn;
         private System.Windows.Forms.Button NextQ;
         private System.Windows.Forms.Button Submit_Exam_Btn;
+        private System.Windows.Forms.Label TimerLabel;
     }
 }
--- The Box v0.1/StudentGui/StudentExamForm.cs
+++ The Box v0.1/StudentGui/StudentExamForm.cs
@@ -16,9 +16,49 @@ namespace The_Box_v0._1.StudentGui
         Student student;
         List<GroupBox> Groupsbox = new List<GroupBox>();
         int iteratorOfQ = 0;
+        
         List<GetRandomExam_Result> Exam = new List<GetRandomExam_Result>();
         List<string> QAnswers = new List<string>();
         string QAnswer;
+        int DurationForExam = 0;
+        Timer TimerPerExam = new Timer();
+        Timer TimerForEachSecond = new Timer();
+
+
+
+
+
+
+        private void TimerElapsedforExam(object sender, EventArgs e)
+        {
+
+            TimerPerExam.Stop();
+            TimerForEachSecond.Stop();
+
+            foreach (var box in Groupsbox)
+            {
+                box.Visible = false;
+
+            }
+            TimerLabel.Visible = false;
+            Submit_Exam_Btn.Visible = false;
+            NextQ.Visible = false;
+
+
+        }
+        private void DecreaseOneSecondInExamTimer(object sender, EventArgs e)
+        {
+            DurationForExam = DurationForExam - 1;
+            TimeSpan t = TimeSpan.FromSeconds(DurationForExam);
+
+            string DurationWithTimeFormat = string.Format("{0:D2}h:{1:D2}m:{2:D2}s",
+                  t.Hours,
+                  t.Minutes,
+                  t.Seconds
+                );
+            TimerLabel.Text = DurationWithTimeFormat;
+
+        }
         public StudentExamForm()
         {
             InitializeComponent();
@@ -40,8 +80,37 @@ namespace The_Box_v0._1.StudentGui
         }
         private void Start_Exam_Btn_Click(object sender, EventArgs e)
         {
+             
+
+           
+
+
             Exam = Ent.ShowExam(ChooseCourse.Text, Groupsbox, this);
+            int examId = Exam[0].E_ID;
+            var durationPerMins = (from ex in Ent.Exams
+                     where ex.E_ID == examId
+                     select ex.E_Duration).FirstOrDefault();
+            DurationForExam = (Int32)durationPerMins * 60;
+            timerConfig((Int32)DurationForExam);
+
         }
+        private void timerConfig(int timeInSecond)
+
+        {
+
+
+            TimerPerExam.Interval = timeInSecond* 1000;
+            TimerPerExam.Tick += new EventHandler(TimerElapsedforExam);
+        TimerPerExam.Start();
+
+
+            TimerForEachSecond.Interval = 1000;
+            TimerForEachSecond.Tick += new EventHandler(DecreaseOneSecondInExamTimer);
+        TimerForEachSecond.Start();
+        }
+
+
+
         private void NextQ_Click(object sender, EventArgs e)
         {
             foreach (GroupBox boxIterator in Groupsbox)
@@ -66,6 +135,11 @@ namespace The_Box_v0._1.StudentGui
                 Ent.ExamAnswer(student.Std_ID, Exam[i].E_ID, Exam[i].Q_ID, QAnswers[i]);
             }
             Ent.ExamCorrection(Exam[0].E_ID, student.Std_ID);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
